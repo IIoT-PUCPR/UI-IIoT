f9ac127680d2356510679dd9ac025bf1b71d2d75
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-06-19 21:20:30 -0300

    updated shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './components/header/header.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule
+  ],
+  exports: [
+    MatIconModule,
+    HeaderComponent,
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class SharedModule { }
