5e1351418a328f619d4ddba3a9dbad58a0ffdd55
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-06-19 21:17:34 -0300

    added asset consumption component

---
+++ src/app/asset/components/asset-details/components/asset-consumption/asset-consumption.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-consumption',
+  templateUrl: './asset-consumption.component.html',
+  styleUrls: ['./asset-consumption.component.scss'],
+  // host: {class: 'full-size'}
+})
+export class AssetConsumptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
